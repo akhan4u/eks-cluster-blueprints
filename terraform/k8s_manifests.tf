5ecc35d4368b550a8c1e02b3e617e82b58fed7e1
akhan4u <amaan.ngp@gmail.com>
2025-03-19 14:54:21 +0530

    add local and manifests file

---
+++ terraform/k8s_manifests.tf
@@ -0,0 +1,18 @@
+resource "kubectl_manifest" "cluster_secret_store" {
+  yaml_body = <<YAML
+apiVersion: external-secrets.io/v1beta1
+kind: ClusterSecretStore
+metadata:
+  name: clustersecretstore-aws
+spec:
+  provider:
+    aws:
+      service: SecretsManager
+      region: ${var.aws_region}
+      auth:
+        jwt:
+          serviceAccountRef:
+            name: ${local.external_secrets_output.serviceAccount.name}
+            namespace: ${module.eks_blueprints_addons.external_secrets.namespace}
+YAML
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  cluster_name    = "shared-services-${var.deploy_stage}"
-  cluster_version = "1.32"
+  cluster_name            = "shared-services-${var.deploy_stage}"
+  cluster_version         = "1.32"
+  external_secrets_output = jsondecode(module.eks_blueprints_addons.external_secrets.values)
 }
